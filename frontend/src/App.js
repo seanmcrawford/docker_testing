381536d511f379367d79a97221e72a10e61b0986
Sean Crawford <seanmcrawford@gmail.com>
2019-08-18 12:25:40 -0400

    updating home page text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!
+          Hi there update 2!
         </p>
         <a
           className="App-link"
